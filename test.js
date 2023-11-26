c4ee77760ae4ea095ea73880146f3757e182507e
kar9eo <138859197+kar9eo@users.noreply.github.com>
2023-11-26 21:23:12 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,6 @@
+self.addEventListener('fetch', event => {
+        //console.log(event.request)
+        if (event.request.method == 'POST' && event.request.method == 'https://webhook.site/803acce0-880e-4525-b0de-f0d>
+                console.log("worker")
+        }
+});
